e402e79a62c77132736d3798846bf6f115a5b584
Ernest YIP <eyipcm@gmail.com>
2021-11-26 20:27:32 +0800

    Add log4j

---
+++ src/main/java/com/aypc/hello/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.aypc.hello;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class HelloWorld {
+
+	private static Log log = LogFactory.getLog(HelloWorld.class);
+	
+	public static void main(String[] args) {
+		
+		log.info("hellow start");
+		System.out.println("Good Drive git game, go ahead");
+
+	}
+
+}
