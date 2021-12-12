7eb1ca44773c4409e5bb9096f682f54f6a438b1f
Ernest YIP <eyipcm@gmail.com>
2021-12-12 22:28:29 +0800

    upgrade logger to log4j2

--- README.md
+++ README.md
@@ -1,6 +1,67 @@
-# hello-world
-learning the github usage by hello world project
+# Upgrade log4j2 replacing log4j info
 
-What ever, save time to push or pull src on internet and write code everywhere.
+ + log4j2 maven version 2.15.0
 
-Work start from scratch
+````maven
+	<dependency>
+		<groupId>org.apache.logging.log4j</groupId>
+		<artifactId>log4j-api</artifactId>
+		<version>2.15.0</version>			
+	</dependency>
+	<dependency>
+		<groupId>org.apache.logging.log4j</groupId>
+		<artifactId>log4j-core</artifactId>
+		<version>2.15.0</version>
+	</dependency>	
+````
+
+ + Maven check dependencies
+
+````cmd
+ mvn dependency:tree
+ 
+[INFO] Scanning for projects...
+[INFO] 
+[INFO] ----------------------< hello-world:hello-world >-----------------------
+[INFO] Building hello-world 0.0.1-SNAPSHOT
+[INFO] --------------------------------[ jar ]---------------------------------
+[INFO] 
+[INFO] --- maven-dependency-plugin:2.8:tree (default-cli) @ hello-world ---
+[INFO] hello-world:hello-world:jar:0.0.1-SNAPSHOT
+[INFO] +- org.apache.logging.log4j:log4j-api:jar:2.15.0:compile
+[INFO] +- org.apache.logging.log4j:log4j-core:jar:2.15.0:compile
+[INFO] +- org.apache.logging.log4j:log4j-1.2-api:jar:2.15.0:compile
+[INFO] +- org.apache.logging.log4j:log4j-slf4j-impl:jar:2.15.0:compile
+[INFO] |  \- org.slf4j:slf4j-api:jar:1.7.25:compile
+[INFO] +- commons-logging:commons-logging:jar:1.2:compile
+[INFO] +- org.apache.logging.log4j:log4j-jcl:jar:2.15.0:compile
+[INFO] \- org.apache.logging.log4j:log4j-jul:jar:2.15.0:compile
+[INFO] ------------------------------------------------------------------------
+[INFO] BUILD SUCCESS
+[INFO] ------------------------------------------------------------------------
+[INFO] Total time: 0.859 s
+[INFO] Finished at: 2021-12-12T21:48:36+08:00
+[INFO] ------------------------------------------------------------------------
+````
+
+ + Exclude conflicting dependencies
+
+	| log4j Dependency | Dependencies to Exclude1 | Exclude2 | Exclude3 |
+	| :--- | :--- | :--- | :--- |
+	| [log4j-1.2-api](log4j-1.2-api) | log4j:log4j | org.slf4j:log4j-over-slf4j ||
+	| [log4j-core](log4j-core) | log4j:log4j | ch.qos.logback:logback-core | org.apache.logging.log4j:log4j-to-slf4j |
+	| [log4j-jcl](log4j-jcl) | org.slf4j:jcl-over-slf4j |||
+	| [log4j-jul](log4j-jul) | org.slf4j:jul-to-slf4j |||
+	| [log4j-slf4j-impl](log4j-slf4j-impl) | org.apache.logging.log4j:log4j-to-slf4j| ch.qos.logback:logback-core ||
+
+ + Reference
+
+[log4j2 FAQ](https://logging.apache.org/log4j/2.x/faq.html)
+
+[log4j2 VS jul LOG level](https://logging.apache.org/log4j/2.x/log4j-jul/index.html)
+	
+[slf4j manual](http://www.slf4j.org/manual.html)
+
+[java-util-logging Level.FINE no show](https://stackoverflow.com/questions/6315699/why-are-the-level-fine-logging-messages-not-showing)
+	
+	
--- src/main/java/com/aypc/hello/HelloWorld.java
+++ src/main/java/com/aypc/hello/HelloWorld.java
@@ -1,17 +1,57 @@
 package com.aypc.hello;
 
+//import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class HelloWorld {
 
-	private static Logger log = LogManager.getLogger(HelloWorld.class);
+	private static org.apache.logging.log4j.Logger log4j2 = LogManager.getLogger(HelloWorld.class);
+	private static org.slf4j.Logger slf4j = org.slf4j.LoggerFactory.getLogger(HelloWorld.class);
+	private static org.apache.commons.logging.Log jcl = LogFactory.getLog(HelloWorld.class);			
+	private static java.util.logging.Logger jul = java.util.logging.Logger.getLogger(Thread.currentThread().getStackTrace()[0].getClassName());
 	
 	public static void main(String[] args) {
 		
-		log.info("hellow start");
+		log4j2.info("log4j2 -- info level hellow start");
+		slf4j.info("slf4j over log4j2 via log4j-1.2-api -- info level hellow start");
+		jcl.info("common-loggging via log4j-1.2-api -- info level hellow start");
+		jul.info("java-util-loggging via log4j-1.2-api -- info level hellow start");
 		System.out.println("Good Drive git game, go ahead");
-		log.info("hellow end");
+		
+		log4j2.debug("log4j2 -- debug level hellow start");
+		slf4j.debug("slf4j over log4j2 via log4j-1.2-api -- debug level hellow start");
+		jcl.debug("common-loggging via log4j-1.2-api -- debug level hellow start");
+		jul.fine("java-util-loggging via log4j-1.2-api -- fine level hellow start");
+		jul.finer("java-util-loggging via log4j-1.2-api -- finer level hellow start");
+		System.out.println("Good Drive git game, go ahead");
+		
+		log4j2.warn("log4j2 -- warn level hellow start");
+		slf4j.warn("slf4j over log4j2 via log4j-1.2-api -- warn level hellow start");
+		jcl.warn("common-loggging via log4j-1.2-api -- warn level hellow start");
+		jul.warning("java-util-loggging via log4j-1.2-api -- warning level hellow start");
+		System.out.println("Good Drive git game, go ahead");
+		
+		log4j2.trace("log4j2 -- trace level hellow start");
+		slf4j.trace("slf4j over log4j2 via log4j-1.2-api -- trace level hellow start");
+		jcl.trace("common-loggging via log4j-1.2-api -- trace level hellow start");		
+		jul.finest("java-util-loggging via log4j-1.2-api -- finest level hellow start");
+		System.out.println("Good Drive git game, go ahead");
+		
+		log4j2.fatal("log4j2 -- fatal level hellow start");
+		//slf4j.fatal("slf4j over log4j2 via log4j-1.2-api -- fatal level hellow start");		
+		jcl.fatal("common-loggging via log4j-1.2-api -- fatal level hellow start");
+		//jul.fatal("java-util-loggging via log4j-1.2-api -- fatal level hellow start");
+		System.out.println("Good Drive git game, go ahead");
+		
 	}
 
 }
